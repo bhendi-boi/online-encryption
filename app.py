1393030f5ed2b51f863eff898af13719d7f4ce4d
bhendi-boi <bhendiisgreen@gmail.com>
2024-02-24 07:54:50 +0530

    removed unnecessary imports

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms.validators import ValidationError, DataRequired
 from wtforms import TextAreaField, SubmitField
@@ -6,22 +6,29 @@ from cryptography.fernet import Fernet
 import rsa
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
-from base64 import b64decode, b64encode
+from base64 import b64decode
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = 'secret'
+app.config["SECRET_KEY"] = "secret"
+
 
 class EncryptionForm(FlaskForm):
-    issue = TextAreaField("Issue", render_kw={"rows": 10, "cols": 50}, validators=[DataRequired()], default="Team Name : \nFile Name : \nBug Description : \nSolution : ")
+    issue = TextAreaField(
+        "Issue",
+        render_kw={"rows": 10, "cols": 50},
+        validators=[DataRequired()],
+        default="Team Name : \nFile Name : \nBug Description : \nSolution : ",
+    )
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     form = EncryptionForm()
     if form.is_submitted():
         result = request.form
-        print("Form is submitted\n", result['issue'])
+        print("Form is submitted\n", result["issue"])
 
-        message = result['issue']
+        message = result["issue"]
         key = Fernet.generate_key()
         fernet = Fernet(key)
 
@@ -40,8 +47,9 @@ def index():
         print(encMessage)
 
         output = [encrypted_key, encMessage]
-        return render_template('result.html', output=output)
-    return render_template('index.html', form=form)
+        return render_template("result.html", output=output)
+    return render_template("index.html", form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
